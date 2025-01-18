86a3953cbbcb82a48ccda6e6c0a1455a23177f92
David S Frech <dfrech@hotmail.com>
2025-01-18 14:41:35 -0500

    Create pySql.py
    Use the my-test.db to run sql queries

---
+++ pySql.py
@@ -0,0 +1,12 @@
+import sqlite3 as sl
+import sys
+
+con = sl.connect('my-test.db')
+
+with con:
+    data = con.execute("SELECT * FROM WORDS WHERE count > 200 ORDER BY count")
+    for row in data:
+        print(row)
+    data = con.execute("SELECT * FROM WORDS WHERE count > 200 ORDER BY word")
+    for row in data:
+        print(row)
